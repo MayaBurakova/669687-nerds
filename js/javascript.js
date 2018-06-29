46d4214d3d3f6ed28500be9f346358cc8103328f
MayaBurakova <39371537+MayaBurakova@users.noreply.github.com>
2018-06-29 14:05:43 +0300

    Ресайз css js, форма

--- js/javascript.js
+++ js/javascript.js
@@ -2,13 +2,13 @@
 	var popup = document.querySelector('.modal');
 	var close = popup.querySelector('.modal-close');
 	var form = popup.querySelector('form');
-	var name = popup.querySelector('[name=name]');
+	var userName = popup.querySelector('[name=name]');
 	var email = popup.querySelector('[name=email]');
 	var isStorageSupport = true;
 	var storage = '';
 	
 	try {
-		storage = localStorage.getItem('name');
+		storage = localStorage.getItem('userName');
 	} catch (err) {
 		isStorageSupport = false;
 	}
@@ -17,10 +17,10 @@
 		evt.preventDefault();
 		popup.classList.add('modal-show');
 		if (storage) {
-			name.value = storage;
+			userName.value = storage;
 			email.focus();
 		} else {
-			name.focus();
+			userName.focus();
 		}
 	});
 	close.addEventListener('click', function (evt) {
@@ -29,16 +29,16 @@
 		popup.classList.remove('modal-error');
 	});
 	form.addEventListener('submit', function (evt) {
-		if (!name.value || !email.value) {
+		if (!userName.value || !email.value) {
 			evt.preventDefault();
 			popup.classList.remove('modal-error');
 			popup.offsetWidth = popup.offsetWidth;
 			popup.classList.add('modal-error');
 		} else {
 			if (isStorageSupport) {
-				localStorage.setItem('name', name.value);
+				localStorage.setItem('userName', userName.value);
 		}
-	});
+	}});
 	window.addEventListener('keydown', function (evt) {
 		if (evt.keyCode === 27) {
 			evt.preventDefault();
